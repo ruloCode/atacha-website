c969933d449f20748a94a2ee47fa207970ceb3bc
Rulo <andres@vitau.mx>
2023-11-16 16:49:53 -0500

    landing

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,8 @@
-import { Lusitana ,Montserrat, Inter } from 'next/font/google'
+import exp from 'constants';
+import { Lusitana ,Montserrat, Inter, Montserrat_Alternates, Open_Sans } from 'next/font/google'
 
+export const open_sans = Open_Sans({ subsets: ['latin'] });
 export const inter = Inter({ subsets: ['latin'] });
 export const montserrat = Montserrat({subsets: ['latin']});
 export const lusitana = Lusitana({ weight: ['400','700'], subsets: ['latin']})
+export const montserrat_alternates = Montserrat_Alternates({weight: ['400','700'], subsets: ['latin']});
